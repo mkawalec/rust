406a841e627809062059f54848f78f898a44bc81
Michał Kawalec <michal@monad.cat>
2023-04-07 13:18:07 +0200

    now with actual parsing

--- compiler/rustc_builtin_macros/src/layout_optimizations.rs
+++ compiler/rustc_builtin_macros/src/layout_optimizations.rs
@@ -1,6 +1,7 @@
-
+use rustc_ast::ptr::P;
 use rustc_ast::tokenstream::TokenStream;
-use rustc_ast::{token, TyKind};
+use rustc_ast::{token, TyKind, Ty, Pat, PatKind};
+use rustc_errors::PResult;
 use rustc_expand::base::{self, *};
 use rustc_parse::parser::{RecoverColon, RecoverComma, CommaRecoveryMode};
 use rustc_span::Span;
@@ -11,19 +12,35 @@ pub fn expand<'cx>(
     span: Span,
     tts: TokenStream,
 ) -> Box<dyn base::MacResult + 'cx> {
-    warn!("foobarz2");
+    let (ty, pat) = match parse_pattern_type(cx, span, tts) {
+        Ok(parsed) => parsed,
+        Err(mut err) => {
+            err.emit();
+            return DummyResult::any(span);
+        }
+    };
+
+    let pattern_ty = cx.ty(span, TyKind::Pat(ty, pat));
+    return MacEager::ty(pattern_ty);
+}
+
+fn parse_pattern_type<'a>(cx: &mut ExtCtxt<'a>, span: Span, tts: TokenStream) -> PResult<'a, (P<Ty>, P<Pat>)> {
     let mut parser = cx.new_parser_from_tts(tts);
 
-    let ty = parser.parse_ty().unwrap();
-    parser.expect(&token::Comma).unwrap();
-    let expr = parser.parse_pat_allow_top_alt(None, RecoverComma::No, RecoverColon::No, CommaRecoveryMode::LikelyTuple).unwrap();
+    let ty = parser.parse_ty()?;
+    parser.expect(&token::Comma)?;
+    let pattern = parser.parse_pat_allow_top_alt(None, RecoverComma::No, RecoverColon::No, CommaRecoveryMode::LikelyTuple).unwrap();
 
-    let layout_ty = cx.ty(span, TyKind::Pat(ty, expr));
-    return MacEager::ty(layout_ty);
-    //let layout_expr = cx.expr(span(ExprKind::Type()))
-    //compile_error!("We executed the macro!".concat(format!("{:?}, {:?}", meta_item, item)));
+    let PatKind::Range(..) = pattern.kind else {
+        let mut err = cx.struct_span_err(span, "Pattern types only support range expressions");
+        err.span_suggestion(
+            parser.token.span,
+            "try supplying a range expression instead",
+            "",
+            rustc_errors::Applicability::MaybeIncorrect
+        );
+        return Err(err);
+    };
 
-    // Looks like we can go ast::Item -> kind -> Struct::VariantData (0) -> FieldDef
-    // to access the type on both Structs and Tuples and set the TyKind
-    //DummyResult::any(span)
+    Ok((ty, pattern))
 }
--- tests/ui/macros/layout-optimizations.rs
+++ tests/ui/macros/layout-optimizations.rs
@@ -1,6 +1,10 @@
-// run-pass
 #![feature(pattern_types)]
+#![allow(incomplete_features)]
 
 fn main() {
-    let blah: layout_optimizations!(u32, 123u32..) = 256;
+    type Wrong = layout_optimizations!(u32, 1 | 2);
+    //~^ Pattern types only support range expressions
+    type Percent = layout_optimizations!(u32, 0..=100);
+    //~^ pattern types are unstable
+    let blah: layout_optimizations!(u32, 123..) = 256;
 }
